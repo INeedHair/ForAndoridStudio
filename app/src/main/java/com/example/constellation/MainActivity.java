a285475282bdd7ca46ec3e8522950c5d3488cb93
INeedHair <644542731@qq.com>
2020-12-14 18:09:27 +0800

    第二次提交 完成了界面跳转。

--- app/src/main/java/com/example/constellation/MainActivity.java
+++ app/src/main/java/com/example/constellation/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.constellation;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.widget.RadioGroup;
@@ -34,22 +35,52 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     private void addFragmentPage() {
         //1.创建碎片管理者对象
         manager = getSupportFragmentManager();
+        //2.创建碎片处理事务的对象
+        FragmentTransaction transaction = manager.beginTransaction();
+        //3.将四个FRAGMENT统一添加到布局
+        transaction.add(R.id.main_layout_center,starFrag);
+        transaction.add(R.id.main_layout_center,partnerFrag);
+        transaction.add(R.id.main_layout_center,luckFrag);
+        transaction.add(R.id.main_layout_center,meFrag);
+        //4.第一次只显示一个所以要隐藏其余的三个
+        transaction.hide(partnerFrag);
+        transaction.hide(luckFrag);
+        transaction.hide(meFrag);
+        //5.提交碎片改变后的事务
+        transaction.commit();
     }
 
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
+        FragmentTransaction transaction = manager.beginTransaction();
         switch (checkedId){
             case R.id.main_radioButton_star:
-
+                transaction.hide(partnerFrag);
+                transaction.hide(luckFrag);
+                transaction.hide(meFrag);
+                transaction.show(starFrag);
+                transaction.commit();
                 break;
             case R.id.main_radioButton_parnter:
-
+                transaction.show(partnerFrag);
+                transaction.hide(luckFrag);
+                transaction.hide(meFrag);
+                transaction.hide(starFrag);
+                transaction.commit();
                 break;
             case R.id.main_radioButton_luck:
-
+                transaction.hide(partnerFrag);
+                transaction.show(luckFrag);
+                transaction.hide(meFrag);
+                transaction.hide(starFrag);
+                transaction.commit();
                 break;
             case R.id.main_radioButton_me:
-
+                transaction.hide(partnerFrag);
+                transaction.hide(luckFrag);
+                transaction.show(meFrag);
+                transaction.hide(starFrag);
+                transaction.commit();
                 break;
 
         }
