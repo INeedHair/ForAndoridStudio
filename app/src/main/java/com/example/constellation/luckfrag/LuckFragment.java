fd06772965a0de7f99a24d5279ac92852cd9e601
INeedHair <644542731@qq.com>
2020-12-24 17:00:04 +0800

    第七次提交 完成了第三个页面的第一级页面的布局。

--- app/src/main/java/com/example/constellation/luckfrag/LuckFragment.java
+++ app/src/main/java/com/example/constellation/luckfrag/LuckFragment.java
@@ -8,26 +8,40 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.example.constellation.R;
+import com.example.constellation.bean.StarBean;
+
+import java.util.List;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * 运势界面
  */
 public class LuckFragment extends Fragment {
-
-
-    public LuckFragment() {
-        // Required empty public constructor
-    }
-
+    GridView luckGv;
+    List<StarBean.StarinfoBean> mDatas;
+    private LuckBaseAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_luck, container, false);
+
+        View view =inflater.inflate(R.layout.fragment_luck, container, false);
+        luckGv=view.findViewById(R.id.luckfrag_gv);
+        //        获取数据源
+        Bundle bundle = getArguments();
+        StarBean infobean = (StarBean) bundle.getSerializable("info");
+        mDatas = infobean.getStarinfo();
+
+        //       创建适配器对象
+        adapter = new LuckBaseAdapter(getContext(), mDatas);
+//        设置适配器
+        luckGv.setAdapter(adapter);
+
+        return view;
     }
 
 }
