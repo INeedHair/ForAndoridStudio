16c838b1afac8d0f3afee87b05013d489e219ed9
INeedHair <644542731@qq.com>
2020-12-25 17:42:07 +0800

    第九次提交 完成了第四个页面的所有功能。

---
+++ app/src/main/java/com/example/constellation/home/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.constellation.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.constellation.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
